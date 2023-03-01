b827ef275a744873f8f436ee762a543f05b1ac16
John Ammon <ammonj78@gmail.com>
2023-03-01 12:43:01 -0700

    Moved Header out of beers

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header(){
     return(
-        <div>
+        <div className="header-container">
             <img className="beer-taps-img" src="public/beerTaps.png" alt="Beer Taps"/>
-            <h1>Home Brew</h1>
+            <h1 className='Header'>Home Brew</h1>
         </div>
     )
 }
