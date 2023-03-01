fe4fe239eca00c21ab183687fcbe3cb786fc52bf
John Ammon <ammonj78@gmail.com>
2023-03-01 09:11:02 -0700

    Added component did mount

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './components/Header/Header'
 import Beers from './components/Beers/Beers'
+import cleanBeerData from './utilities';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      < Header />
-      < Beers />
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super()
+    this.sate = {
+      beers: []
+    }
+  }
+
+ComponentDidMount(){
+  getApiData(beers)
+  .then(data => {
+    const cleanBeersData = data.map(beer => cleanBeerData(beer))
+    this.setState({beers: cleanBeersData})
+  })
+}
+
+  render(){
+    return (
+      <div className="App">
+          < Header />
+          < Beers />
+      </div>
+    )
+  }
 }
 
 export default App;
