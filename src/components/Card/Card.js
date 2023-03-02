0fc510e5821df5ac014e0c14c0f25af598636e10
John Ammon <ammonj78@gmail.com>
2023-03-01 18:53:19 -0700

    Made the beers dynamic

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,7 +6,7 @@ const Card = ({ id, name, tagline, description, beerImage }) => {
   return (
         <div className='beer-cards'>
             <span className='beer-image-span'>
-            <img className="beer-image" src={beerImage} alt={'img'} />
+                <img className="beer-image" src={beerImage} alt={'img'} />
             </span>
             <span className='beer-info'>
                 <h3>{name}</h3>
