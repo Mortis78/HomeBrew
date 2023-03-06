b4d238bcff812715addb7330577038e1960b4054
John Ammon <ammonj78@gmail.com>
2023-03-05 20:04:06 -0700

    Deploy React app to GitHub Pages

---
+++ precache-manifest.5754860438529fee9494fdedfc22cb17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34ec3d81979fc6ad2abe",
+    "url": "/HomeBrew/static/css/main.0f9e551a.chunk.css"
+  },
+  {
+    "revision": "34ec3d81979fc6ad2abe",
+    "url": "/HomeBrew/static/js/main.34ec3d81.chunk.js"
+  },
+  {
+    "revision": "a55ae8da76b6c0c45aec",
+    "url": "/HomeBrew/static/js/1.a55ae8da.chunk.js"
+  },
+  {
+    "revision": "fe03d149c0985101647e",
+    "url": "/HomeBrew/static/js/2.fe03d149.chunk.js"
+  },
+  {
+    "revision": "5effd808e60c2945059d",
+    "url": "/HomeBrew/static/js/runtime~main.5effd808.js"
+  },
+  {
+    "revision": "eefaf5d84f65d9ef6a8f61d629c2865f",
+    "url": "/HomeBrew/static/media/beerTaps.eefaf5d8.png"
+  },
+  {
+    "revision": "d69955f752484ae4c7d717380b5d9141",
+    "url": "/HomeBrew/index.html"
+  }
+];
